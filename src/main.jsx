d0da03a2cff57d95cb2ca077b59e2c776af6ff61
ConnectPrakash <pp3662504@gmail.com>
2025-04-02 10:43:24 +0530

    ninth commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import ErrorBoundary from "./component/ErrorBoundary"; // Import the error boundary
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </BrowserRouter>
+  </React.StrictMode>
+);
